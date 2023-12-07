cbd42182fb36b267e913df1fca4094d014045bb1
Yi Chen <chenyiapp@yahoo.com>
2023-12-07 12:46:36 -0800

    remove firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,33 +1,9 @@
 
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { getStorage } from 'firebase/storage';
-import { getFirestore } from 'firebase/firestore/lite';
+// import { initializeApp } from "firebase/app";
+// import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+// import { getStorage } from 'firebase/storage';
+// import { getFirestore } from 'firebase/firestore/lite';
 
-
-const firebaseConfig = {
-        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-        appId:process.env.REACT_APP_FIREBASE_APP_ID
-    }
-
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-
-
-export const signInWithGoogle = () => signInWithPopup(auth, provider);
-
-
-// import firebase from "firebase/app";
-// import "firebase/auth"
 
 // const firebaseConfig = {
 //         apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -37,21 +13,45 @@ export const signInWithGoogle = () => signInWithPopup(auth, provider);
 //         messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 //         appId:process.env.REACT_APP_FIREBASE_APP_ID
 //     }
-// const app = firebase.initializeApp(firebaseConfig)
-
-// export const auth =  app.auth()
-// export default app
 
 
+// const app = initializeApp(firebaseConfig);
+
+// export const auth = getAuth(app);
+// const provider = new GoogleAuthProvider();
+// export const db = getFirestore(app);
+// export const storage = getStorage(app);
+
+
+// export const signInWithGoogle = () => signInWithPopup(auth, provider);
+
+
+// // import firebase from "firebase/app";
+// // import "firebase/auth"
+
+// // const firebaseConfig = {
+// //         apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+// //         authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+// //         projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+// //         storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+// //         messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+// //         appId:process.env.REACT_APP_FIREBASE_APP_ID
+// //     }
+// // const app = firebase.initializeApp(firebaseConfig)
+
+// // export const auth =  app.auth()
+// // export default app
 
 
 
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAv9E_HWlwgqC9AXDAt44WIuN-h9xTp-_8",
-//     authDomain: "portfolio-5086f.firebaseapp.com",
-//     projectId: "portfolio-5086f",
-//     storageBucket: "portfolio-5086f.appspot.com",
-//     messagingSenderId: "264829656766",
-//     appId: "1:264829656766:web:577ef5dc6859710951c8ef"
-// };
+
+
+// // const firebaseConfig = {
+// //     apiKey: "AIzaSyAv9E_HWlwgqC9AXDAt44WIuN-h9xTp-_8",
+// //     authDomain: "portfolio-5086f.firebaseapp.com",
+// //     projectId: "portfolio-5086f",
+// //     storageBucket: "portfolio-5086f.appspot.com",
+// //     messagingSenderId: "264829656766",
+// //     appId: "1:264829656766:web:577ef5dc6859710951c8ef"
+// // };
